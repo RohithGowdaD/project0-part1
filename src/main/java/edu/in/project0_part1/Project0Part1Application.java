856a8ce3796bbaa5fdd92204449e8b9452004a27
Rohith Gowda Devaraju <rohithgowdadevaraju@gmail.com>
2026-02-28 15:12:40 -0500

    Initial commit

---
+++ src/main/java/edu/in/project0_part1/Project0Part1Application.java
@@ -0,0 +1,13 @@
+package edu.in.project0_part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0Part1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project0Part1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/in/project0_part1/Project0Part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.in.project0_part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project0Part1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
